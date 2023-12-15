ad829dad32964f7bb43a99907b3eefe6828f5a67
Diego Añazco <danazco@vozy.co>
2023-12-15 12:35:02 -0500

    :sparkles: Router and styles login

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
-import DashboardPage from './pages/dashboard'
-import LoginPage from './pages/login'
- 
-import './App.css'
+import DashboardPage from "./pages/dashboard"
+import LoginPage from "./pages/login"
+
+import "./App.css"
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<DashboardPage />}/>
-        <Route path='/login' element={<LoginPage />}/>
+        <Route index element={<DashboardPage />} />
+        <Route path='/' element={<DashboardPage />} />
+        <Route path='/login' element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/login/styles.ts
@@ -0,0 +1,20 @@
+export const mainContainerStyles = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+}
+
+export const loginCardStyles = {
+  padding: '32px 24px',
+  width: 432,
+  boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.15)',
+  height: 'fit-content',
+  cardBody: {
+    padding: 0,
+  },
+  title: {
+    marginTop: 10,
+    marginBottom: 10,
+  }
+}
