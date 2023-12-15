440360cd0460e3eb824ef0678dfce884332d12a4
Diego Añazco <danazco@vozy.co>
2023-12-15 13:17:36 -0500

    :lipstick: Adding font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { ThemeProvider } from '@mui/material/styles'
 
 import DashboardPage from "./pages/dashboard"
 import LoginPage from "./pages/login"
+
+import theme from "./theme"
 
 import "./App.css"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<DashboardPage />} />
-        <Route path='/' element={<DashboardPage />} />
-        <Route path='/login' element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<DashboardPage />} />
+          <Route path='/' element={<DashboardPage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  typography:{
+    fontFamily: [
+      'Montserrat',
+    ].join(','),
+  }
+})
+
+export default theme
